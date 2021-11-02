60571c057fd8067b2c5c8935d346b2fb96329b0d
sidchiku9 <sidchiku9@gmail.com>
2021-11-02 22:51:40 +0530

    Some changes to JS

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const weakPwdOne = /[a-z]/
 const weakPwdTwo = /[0-9]/
 const modPwd = /[a-z0-9]/
 const strongPwd = /[A-Z0-9]/
-const ucPwd = /[A-Z0-9$!_%]/
+const ucPwd = /^(?=.*[A-Z\d].*)[A-Z\d\!@#\$%&\*]{1,}$/
 
 function testFun() {
     if ((weakPwdOne.test(pwdInput.value) && !weakPwdTwo.test(pwdInput.value)) || (!weakPwdOne.test(pwdInput.value) && weakPwdTwo.test(pwdInput.value))) {
@@ -12,16 +12,16 @@ function testFun() {
         document.getElementById("pwdbar").style.backgroundColor = "red";
         document.getElementById("pwdnumber").innerHTML = "20%";
         document.getElementById("pwdtitle").innerHTML = "Weak Password";
-    } else if (modPwd.test(pwdInput.value)) {
-        document.getElementById("pwdbar").style.width = "40%";
-        document.getElementById("pwdbar").style.backgroundColor = "Orange";
-        document.getElementById("pwdnumber").innerHTML = "40%";
-        document.getElementById("pwdtitle").innerHTML = "Moderate Password";
     } else if (strongPwd.test(pwdInput.value)) {
         document.getElementById("pwdbar").style.width = "60%";
         document.getElementById("pwdbar").style.backgroundColor = "Blue";
         document.getElementById("pwdnumber").innerHTML = "60%";
         document.getElementById("pwdtitle").innerHTML = "Strong Password";
+    } else if (modPwd.test(pwdInput.value)) {
+        document.getElementById("pwdbar").style.width = "40%";
+        document.getElementById("pwdbar").style.backgroundColor = "Orange";
+        document.getElementById("pwdnumber").innerHTML = "40%";
+        document.getElementById("pwdtitle").innerHTML = "Moderate Password";
     } else if (ucPwd.test(pwdInput.value)) {
         document.getElementById("pwdbar").style.width = "80%";
         document.getElementById("pwdbar").style.backgroundColor = "Green";
@@ -29,5 +29,8 @@ function testFun() {
         document.getElementById("pwdtitle").innerHTML = "Uncrackable Password";
     } else {
         document.getElementById("pwdtitle").innerHTML = "Invalid password";
+        document.getElementById("pwdbar").style.width = "0%";
+        document.getElementById("pwdbar").style.backgroundColor = "#FBEEC1";
+        document.getElementById("pwdnumber").innerHTML = "";
     }
 }
