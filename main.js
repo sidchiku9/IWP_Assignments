66b5117e6cc89882538f23b292cba14608d68eae
sidchiku9 <sidchiku9@gmail.com>
2021-11-02 14:28:44 +0530

    Some more changes

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const pwdName = document.getElementById(pwdtitle);
-const pwdNumber = document.getElementById(pwdumber);
-const pwdBar = document.getElementById(pwdbar);
-const pwdInput = document.getElementById(pwdinput);
+let pwdInput = document.getElementById("pwdinput");
 
-const weakPwd = /[a-z]|[A-Z]|[0-9]/;
-const modPwd = /[a-z0-9]/;
-const strongPwd = /[A-Z0-9]/;
-const ucPwd = /[]/;
+const weakPwd = /[a-z | 0-9]*/
+const modPwd = /[a-z0-9]*/
+const strongPwd = /[A-Z0-9]*/
+const ucPwd = /[A-Z0-9$!_%]*/
+
+function testFun() {
+    if (weakPwd.test(pwdInput.value)) {
+        document.getElementById("pwdbar").style.width = "20%";
+        document.getElementById("pwdbar").style.color = "red";
+        document.getElementById("pwdnumber").innerHTML = "20%";
+        document.getElementById("pwdtitle").innerHTML = "Weak Password";
+    } else {
+        document.getElementById("pwdtitle").innerHTML = "Not a weak password";
+    }
+}
