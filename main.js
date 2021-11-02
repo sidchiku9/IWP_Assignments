409aef4a065821d719e56d19d68656ca950d4819
sidchiku9 <sidchiku9@gmail.com>
2021-11-02 14:51:33 +0530

    Algo works now

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 let pwdInput = document.getElementById("pwdinput");
 
-const weakPwd = /[a-z | 0-9]*/
-const modPwd = /[a-z0-9]*/
-const strongPwd = /[A-Z0-9]*/
-const ucPwd = /[A-Z0-9$!_%]*/
+const weakPwdOne = /[a-z]/
+const weakPwdTwo = /[0-9]/
+const modPwd = /[a-z0-9]/
+const strongPwd = /[A-Z0-9]/
+const ucPwd = /[A-Z0-9$!_%]/
 
 function testFun() {
-    if (weakPwd.test(pwdInput.value)) {
+    if ((weakPwdOne.test(pwdInput.value) && !weakPwdTwo.test(pwdInput.value)) || (!weakPwdOne.test(pwdInput.value) && weakPwdTwo.test(pwdInput.value))) {
         document.getElementById("pwdbar").style.width = "20%";
         document.getElementById("pwdbar").style.color = "red";
         document.getElementById("pwdnumber").innerHTML = "20%";
