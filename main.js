8abc81101918cce92136f3c45160cf2f0cfb3b3e
sidchiku9 <sidchiku9@gmail.com>
2021-11-03 16:26:45 +0530

    Final commit

--- main.js
+++ main.js
@@ -1,43 +1,46 @@
 let pwdInput = document.getElementById("pwdinput");
 
-const weakPwd = /[^a-z]+/
-const weakPedTwo = /[^0-9]+/
-const modPwd = /[^a-z0-9]+/
-const strongPwd = /[^A-Z0-9]+{6,}/
+const weakPwd = /^[a-z]/
+const weakPedTwo = /^[\d]/
+const modPwd = /^[a-z\d]{5,}$/
+const strongPwd = /^[A-Z\d]/
 const ucPwd = /^[A-Z\d\!@#\$%&\*]{10,}$/
 
 function testFun() {
-    if (weakPwd.test(pwdInput.value) || weakPedTwo.test(pwdInput.value)) {
+    if (modPwd.test(pwdInput.value)) {
 
-        document.getElementById("pwdbar").style.width = "20px";
+        document.getElementById("pwdbar").style.width = "40%";
+        document.getElementById("pwdbar").style.backgroundColor = "Orange";
+        document.getElementById("pwdnumber").innerHTML = "40%";
+        document.getElementById("pwdtitle").innerHTML = "Moderate Password";
+        document.getElementById("pwdNew").style.display = "block";
+        document.getElementById("newpwd").innerHTML = generatePassword();
+
+    } else if (weakPwd.test(pwdInput.value) || weakPedTwo.test(pwdInput.value)) {
+        document.getElementById("pwdbar").style.width = "20%";
         document.getElementById("pwdbar").style.backgroundColor = "red";
         document.getElementById("pwdnumber").innerHTML = "20%";
         document.getElementById("pwdtitle").innerHTML = "Weak Password";
 
-        //document.getElementById("pwd__New").style.display = "block";
-        document.getElementById("neww__pwd").innerHTML = generatePassword();
+        document.getElementById("pwdNew").style.display = "block";
+        document.getElementById("newpwd").innerHTML = generatePassword();
 
-
-    } else if (modPwd.test(pwdInput.value)) {
-        document.getElementById("pwdbar").style.width = "40px";
-        document.getElementById("pwdbar").style.backgroundColor = "Orange";
-        document.getElementById("pwdnumber").innerHTML = "40%";
-        document.getElementById("pwdtitle").innerHTML = "Moderate Password";
-        //document.getElementById("pwd__New").style.display = "block";
-        document.getElementById("neww__pwd").innerHTML = generatePassword();
 
     } else if (strongPwd.test(pwdInput.value)) {
-        document.getElementById("pwdbar").style.width = "60px";
+
+        document.getElementById("pwdbar").style.width = "60%";
         document.getElementById("pwdbar").style.backgroundColor = "Blue";
         document.getElementById("pwdnumber").innerHTML = "60%";
         document.getElementById("pwdtitle").innerHTML = "Strong Password";
 
     } else if (ucPwd.test(pwdInput.value)) {
-        document.getElementById("pwdbar").style.width = "80px";
+
+        document.getElementById("pwdbar").style.width = "80%";
         document.getElementById("pwdbar").style.backgroundColor = "Green";
         document.getElementById("pwdnumber").innerHTML = "80%";
         document.getElementById("pwdtitle").innerHTML = "Uncrackable Password";
     } else {
+
         document.getElementById("pwdtitle").innerHTML = "Invalid password";
         document.getElementById("pwdbar").style.width = "0px";
         document.getElementById("pwdbar").style.backgroundColor = "#FBEEC1";
