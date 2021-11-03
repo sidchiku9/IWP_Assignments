6a2d17f547644eabf3d421051fae3544baa637cc
sidchiku9 <sidchiku9@gmail.com>
2021-11-03 13:57:16 +0530

    .

--- main.js
+++ main.js
@@ -8,30 +8,30 @@ const ucPwd = /^[A-Z\d\!@#\$%&\*]{10,}$/
 
 function testFun() {
     if (ucPwd.test(pwdInput.value)) {
-        document.getElementById("pwdbar").style.width = "80%";
+        document.getElementById("pwdbar").style.width = "80px";
         document.getElementById("pwdbar").style.backgroundColor = "Green";
         document.getElementById("pwdnumber").innerHTML = "80%";
         document.getElementById("pwdtitle").innerHTML = "Uncrackable Password";
     } else if (modPwd.test(pwdInput.value)) {
-        document.getElementById("pwdbar").style.width = "40%";
+        document.getElementById("pwdbar").style.width = "40px";
         document.getElementById("pwdbar").style.backgroundColor = "Orange";
         document.getElementById("pwdnumber").innerHTML = "40%";
         document.getElementById("pwdtitle").innerHTML = "Moderate Password";
 
     } else if (strongPwd.test(pwdInput.value)) {
-        document.getElementById("pwdbar").style.width = "60%";
+        document.getElementById("pwdbar").style.width = "60px";
         document.getElementById("pwdbar").style.backgroundColor = "Blue";
         document.getElementById("pwdnumber").innerHTML = "60%";
         document.getElementById("pwdtitle").innerHTML = "Strong Password";
 
     } else if (((weakPwdOne.test(pwdInput.value) && !weakPwdTwo.test(pwdInput.value)) || (!weakPwdOne.test(pwdInput.value) && weakPwdTwo.test(pwdInput.value)))) {
-        document.getElementById("pwdbar").style.width = "20%";
+        document.getElementById("pwdbar").style.width = "20px";
         document.getElementById("pwdbar").style.backgroundColor = "red";
         document.getElementById("pwdnumber").innerHTML = "20%";
         document.getElementById("pwdtitle").innerHTML = "Weak Password";
     } else {
         document.getElementById("pwdtitle").innerHTML = "Invalid password";
-        document.getElementById("pwdbar").style.width = "0%";
+        document.getElementById("pwdbar").style.width = "0px";
         document.getElementById("pwdbar").style.backgroundColor = "#FBEEC1";
         document.getElementById("pwdnumber").innerHTML = "";
     }
